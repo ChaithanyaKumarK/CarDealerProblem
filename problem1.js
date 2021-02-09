69fd49946a8fdb004ad62eb338161a7586f741ea
ChaithanyaKumarK <chaithanya.kumar@mountblue.tech>
2021-02-10 00:28:48 +0530

    new version of problem1 where a small logical error with respect to index has been resolved.

--- problem1.js
+++ problem1.js
@@ -3,7 +3,7 @@ function searchById(inventory,id){
     var idObject;
     var idExistFlag= false;
     for (i=0;i<Object.keys(inventory).length;i++){
-        if((id-1)==inventory[i].id){
+        if(id==inventory[i].id){
         idObject=inventory[i];
         idExistFlag=true;
         break;
@@ -12,7 +12,7 @@ function searchById(inventory,id){
     if(idExistFlag==false){
         return "";
     }
-    return "Car "+id+" is a "+idObject.car_year+" "+idObject.car_make+" "+idObject.car_model;
+    return "Car "+idObject.id+" is a "+idObject.car_year+" "+idObject.car_make+" "+idObject.car_model;
 }
 
 module.exports =searchById;
